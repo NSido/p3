9cad72b664afd6ec5d9bb0ad17eebfca179617c5
Hamad Rafi <hamadiqbal46@gmail.com>
2019-11-23 19:30:04 +0100

    layout for login, signup, homescreen

--- app/src/main/java/com/example/p3/MainActivity.java
+++ app/src/main/java/com/example/p3/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.p3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.login_button);
+        TextView signUp = findViewById(R.id.register_textview);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginAcitvity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
 //?
 
